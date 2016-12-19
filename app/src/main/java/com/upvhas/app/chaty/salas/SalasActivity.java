03a59c73bdd77ac86c1964850c7a3c23093a4d62
Upvhas <xlopez3000@gmail.com>
2016-12-19 16:27:44 -0600

    Navigation Drawer con header listo
    La primera versión del navigation drawer que contiene el header y solo funciona la opción de cerrar sesión.

--- app/src/main/java/com/upvhas/app/chaty/salas/SalasActivity.java
+++ app/src/main/java/com/upvhas/app/chaty/salas/SalasActivity.java
@@ -1,31 +1,108 @@
 package com.upvhas.app.chaty.salas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
 import com.upvhas.app.chaty.R;
+import com.upvhas.app.chaty.login.LoginActivity;
 
-public class SalasActivity extends AppCompatActivity {
+public class SalasActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
-    private String[] mTitulos;
     private DrawerLayout drawerLayout;
-    private ListView drawerListView;
+    private NavigationView salasNavigationView;
+
+    // Views
+        // Header navigation drawer
+    private TextView nameUser;
+    private ImageView profileImageUser;
+
+    // Google api client for sign out
+    GoogleApiClient mGoogleApiClient;
+
+    // Firebase Instances
+    FirebaseAuth mFirebaseAuth;
+    FirebaseAuth.AuthStateListener mStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_salas);
 
-        mTitulos = new String[]{"Opción 1", "Opción 2", "Opción 3"};
-        drawerLayout = (DrawerLayout) findViewById(R.id.salas_drawer);
-        drawerListView = (ListView) findViewById(R.id.salas_drawer_Listview);
+        setToolbar();
 
-        drawerListView.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item,R.id.item_drawer_title,mTitulos));
+        // Initialize views
+        drawerLayout = (DrawerLayout) findViewById(R.id.salas_drawer_layout);
 
+
+        // Initialize Navigation view
+        salasNavigationView = (NavigationView) findViewById(R.id.salas_navigation_view);
+        if(salasNavigationView != null){
+            View navView = salasNavigationView.getHeaderView(0);
+            nameUser = (TextView) navView.findViewById(R.id.salas_drawer_name_TextView);
+            profileImageUser = (ImageView) navView.findViewById(R.id.salas_drawer_profile_photo);
+            setupDrawerContent(salasNavigationView);
+        }
+
+
+
+        // Gooogle Sign In Requisito for signOut()
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // Firebase Initialize
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        // Initialize AuthStateListener
+        mStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null){// user signed in
+
+                }else{ // user signed out
+                    startActivity(new Intent(SalasActivity.this, LoginActivity.class));
+                }
+            }
+        };
+
+        Intent intent = getIntent();
+        if(intent != null){
+            nameUser.setText(intent.getStringExtra("displayName"));
+            Log.v("PHOTO_URL_GOOGLE",intent.getStringExtra("photoUrl"));
+            Glide.with(SalasActivity.this)
+                    .load(intent.getStringExtra("photoUrl"))
+                    .into(profileImageUser);
+        }
+
+        // load fragment salas
         if(savedInstanceState == null){
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.activity_salas_container,new SalasFragment())
@@ -33,5 +110,84 @@ public class SalasActivity extends AppCompatActivity {
         }
     } // --> end onCreate
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mStateListener);
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(mStateListener != null){
+            mFirebaseAuth.removeAuthStateListener(mStateListener);
+        }
+    }
+
+    private void signOut() {
+        // Firebase sign out
+        mFirebaseAuth.signOut();
+        // google sign out
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                Toast.makeText(SalasActivity.this,"Log out",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    /**
+     * Handle click for navigation view
+     * @param navigationView is the current in the activity layout
+     */
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()){
+                            case R.id.salas_drawer_action_signout:
+                                signOut();
+                                return true;
+                        }
+                        String title = menuItem.getTitle().toString();
+                        Toast.makeText(SalasActivity.this,title,Toast.LENGTH_LONG).show();
+                        return true;
+                    }
+                }
+        );
+    }
+
+    /**
+     * Set toolbar as actionbar and enabled hamburguer icon click
+     */
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.salas_toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setHomeAsUpIndicator(R.mipmap.ic_dehaze_black_24dp);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
--- app/src/main/java/com/upvhas/app/chaty/salas/SalasFragment.java
+++ app/src/main/java/com/upvhas/app/chaty/salas/SalasFragment.java
@@ -1,153 +1,38 @@
 package com.upvhas.app.chaty.salas;
 
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.firebase.auth.FirebaseAuth;
 import com.upvhas.app.chaty.R;
-import com.upvhas.app.chaty.login.LoginActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SalasFragment extends Fragment implements GoogleApiClient.OnConnectionFailedListener{
-
-    private TextView displayNameTextView;
-    private ImageView profileImageView;
-    private android.support.v7.widget.Toolbar salasToolbar;
-
-    // Google api client for sign out
-    GoogleApiClient mGoogleApiClient;
-
-    // Firebase Instances
-    FirebaseAuth mFirebaseAuth;
-    FirebaseAuth.AuthStateListener mStateListener;
+public class SalasFragment extends Fragment{
 
     public SalasFragment() {
         // Required empty public constructor
-        setHasOptionsMenu(true);
+
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Gooogle Sign In Requisito for signOut()
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
 
-         mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
-                .enableAutoManage(getActivity() /* FragmentActivity */, this /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_salas, container, false);
-        // initialize views
-        displayNameTextView = (TextView) rootView.findViewById(R.id.displayNameTextView);
-        profileImageView = (ImageView) rootView.findViewById(R.id.profileImageView);
-        salasToolbar = (android.support.v7.widget.Toolbar) rootView.findViewById(R.id.salas_toolbar);
-
-        // set the toolbar as action bar
-        ((AppCompatActivity) getActivity()).setSupportActionBar(salasToolbar);
-
-        // Firebase Initialize
-        mFirebaseAuth = FirebaseAuth.getInstance();
-
-        // Initialize AuthStateListener
-        mStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if(firebaseAuth.getCurrentUser() != null){// user signed in
-
-                }else{ // user signed out
-                    startActivity(new Intent(getActivity(), LoginActivity.class));
-                }
-            }
-        };
-
-        Intent intent = getActivity().getIntent();
-        if(intent != null){
-            displayNameTextView.setText(intent.getStringExtra("displayName"));
-             Log.v("PHOTO_URL_GOOGLE",intent.getStringExtra("photoUrl"));
-             Glide.with(profileImageView.getContext())
-                    .load(intent.getStringExtra("photoUrl"))
-                    .into(profileImageView);
-        }
 
         return rootView;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        mFirebaseAuth.addAuthStateListener(mStateListener);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if(mStateListener != null){
-            mFirebaseAuth.removeAuthStateListener(mStateListener);
-        }
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.fragment_salas,menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.action_signOut:
-                signOut();
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void signOut() {
-        // Firebase sign out
-        mFirebaseAuth.signOut();
-        // google sign out
-        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
-            @Override
-            public void onResult(@NonNull Status status) {
-                Toast.makeText(getActivity(),"Log out",Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Toast.makeText(getActivity(),"Google play services error!",Toast.LENGTH_LONG).show();
-    }
 
 }
